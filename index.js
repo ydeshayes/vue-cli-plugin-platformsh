0fe401ef3d84562d3ead17e3f2d3a48b5fe3e29c
ydeshayes <deshayes.yann@gmail.com>
2019-03-13 14:31:19 +0100

    Using chain webpack

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 module.exports = api => {
   api.chainWebpack(webpackConfig => {
-    webpackConfig.externals = {
-       ...webpackConfig.externals,
-      platformsh_variables: 'platformsh_variables'
-    }
+    webpackConfig.externals({
+      'platformsh_variables': 'platformsh_variables'
+    })
   }
 };
